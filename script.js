660909a6b4e126be05d6c935ed4fdf9daff7e148
David Andersson <lejonmanen@gmail.com>
2020-11-20 15:28:02 +0100

    Offline first cat image library

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ if( 'serviceWorker' in navigator ) {
         console.log('Service worker registration error: ', error.message);
     })
 }
+
+
+
+window.addEventListener('load', () => {
+    let catIndex = 0;
+    const cats = ['cat0.jpg', 'cat1.jpg', 'cat2.jpg', 'cat3.jpg', 'cat4.jpg', 'cat5.jpg', 'cat6.jpg'];
+    const nextButton = document.querySelector('#nextButton');
+    const img = document.querySelector('img');
+
+    nextButton.addEventListener('click', () => {
+        catIndex = (catIndex + 1) % cats.length;
+        img.src = 'img/' + cats[catIndex];
+    })
+})
